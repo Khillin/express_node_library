688e6d4985ab24546d35bb2928452666a059699b
khelendra shakit <khelendra.k.shakit@gmail.com>
2022-12-18 19:22:01 +0530

    delete operation

--- index.js
+++ index.js
@@ -71,6 +71,18 @@ function validateCourse(course) {
    return schema.validate(course);
 }
 
+app.delete('/api/courses/:id',(req,res) =>{
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) res.status(404).send(`Course with id ${req.params.id} not found`);
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+})
+
+
 const port = process.env.PORT || 3000;
 
 
