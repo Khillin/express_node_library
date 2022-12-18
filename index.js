9d7b17cd65dda49852506cb9319f0d9eb183ee9b
khelendra shakit <khelendra.k.shakit@gmail.com>
2022-12-18 19:10:34 +0530

    fist commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, title:'Data Structures'},
+    {id: 2, title:'Learn Node JS'},
+    {id: 3, title:'Lear React JS'}
+]
+
+//GET Request
+app.get('/',(req,res) =>{
+    res.send('Welcome to Home Page');
+})
+
+app.get('/api/courses',(req,res) =>{
+    res.send(courses);
+})
+
+app.get('/api/courses/:id',(req,res) =>{
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) res.status(404).send(`Course with id ${req.params.id} not found`);
+    res.send(course);
+})
+
+//POST Request
+app.post('/api/courses',(req,res) =>{
+    console.log("post request trigerred");
+
+    const {error} = validateCourse(req.body);
+
+    if(error){
+        //400 Bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length +1,
+        title: req.body.title
+    };
+    courses.push(course);
+    res.send(course);
+
+})
+
+//PUT request
+app.put('/api/courses/:id',(req,res) =>{
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) res.status(404).send(`Course with id ${req.params.id} not found`);
+
+    const {error} = validateCourse(req.body);
+
+    if(error){
+        //400 Bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.title = req.body.title;
+    res.send(course);
+
+})
+
+function validateCourse(course) {
+
+    const schema = Joi.object({
+        title: Joi.string().min(3).required()
+    })
+   return schema.validate(course);
+}
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(3000,() => console.log(`Listening on port ${port}...`));
